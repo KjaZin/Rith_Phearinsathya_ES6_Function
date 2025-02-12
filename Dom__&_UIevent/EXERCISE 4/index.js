ff74d73b6b3894dcc06ae336ca85fe9da30ed9b1
KjaZin <ysat404@gmail.com>
2025-02-12 21:01:59 +0700

    Update

--- Dom__&_UIevent/EXERCISE 4/index.js
+++ Dom__&_UIevent/EXERCISE 4/index.js
@@ -1,5 +1,6 @@
 // SECRET CODE  ---------------------------------------------------------
 const SECRET_CODE = 2359;
+let attempts = 3;
 
 // DOMS ELEMENTS  ---------------------------------------------------------
 const passwordView = document.getElementById("passwordView");
@@ -13,7 +14,7 @@ const tryAgainButton = document.getElementById("tryAgainButton");
 tryAgainButton.addEventListener("click", showGame);
 
 const passwordInput = document.getElementById("passwordInput");
-passwordInput.addEventListener("keypress", handleEnterPasserord);
+passwordInput.addEventListener("keypress", handleEnterPassword);
 
 const instructionLabel = document.getElementById("instructionLabel");
  
@@ -29,21 +30,46 @@ function show(element) {
 
 function showGame() {
   // You can use this function to dispaly the Game view
+  passwordInput.value = "";
+  instructionLabel.textContent = "Enter your code (only 3 times)";
+  show(passwordView);
+  hide(lostView);
+  hide(wonView);
 }
 
 function showWin() {
   // You can use this function to dispaly the Win View
+  hide(passwordView);
+  show(wonView);
 }
 function showLost() {
     // You can use this function to dispaly the Lost View
- 
+    hide(passwordView);
+    show(lostView);
 }
-
- 
+function handleEnterPassword(event) {
+  if (event.key === "Enter") {
+    handleCheck();
+  }
+}
 function handleCheck() {
      // Manage your logic when the button is pressed
+     const enterCode = passwordInput.value.trim();
+     if (enterCode === SECRET_CODE) {
+      showWin();
+     }
+     else {
+      attempts--;
+      if (attempts > 0) {
+        instructionLabel.textContent = `Wrong code ${attempts} attempt left`;
+      }
+      else {
+        showLost();
+      }
+    }
+    passwordInput.value = "";
 }
 
 // MAIN   ---------------------------------------------------------
-
+showGame();
  
--- Dom__&_UIevent/EXERCISE 5/js/script.js
+++ Dom__&_UIevent/EXERCISE 5/js/script.js
@@ -5,17 +5,28 @@
 function addItem() {
   // 1- Create a new task
   // TODO
-
+  let task = {};
   //  2- Set the description from the text field
   // TODO
-
+  task.description = document.getElementById("description").value;
   // 3- Set the priority from select field
   // TODO
-
+  task.priority = document.getElementById("priority").valu;
   // 4- Add the new object to the array
   // TODO
-
+  tasks.push(task);
   console.log(tasks);
+  displayTasks();
+}
+function displayTasks() {
+  const taskList = document.createElement("ul");
+  document.querySelector(".container").innerHTML = "";
+  tasks.forEach(task => {
+    const taskItem = document.createElement("li");
+    taskItem.textContent = `${task.description} \nPriority: ${task.priority}`;
+    taskList.appendChild(taskItem);
+  });
+  document.querySelector(".container").appendChild(taskList);
 }
 
 // ----------------------------------------------------------------------------
